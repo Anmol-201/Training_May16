9c198079c5474be01ccde006fc14455dee31695c
Anmol <anmolbiswas20@gmail.com>
2024-05-23 14:23:17 +0000

    Arithmetic v1.0

---
+++ arithmetic.c
@@ -0,0 +1,22 @@
+/*
+Author : Anmol Biswas
+BMI calculator
+DOC : 23rd May 2024
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter 2 numbers: ");
+    scanf("%d %d",&a,&b);
+    printf("Arithmetic operations are: \n");
+    printf("Addition : %d\n",a+b);
+    printf("Subtraction : %d\n",a-b);
+    printf("Multiplication : %d\n",a*b);
+    printf("Division : %d",a/b);
+
+    return 0;
+}
+
