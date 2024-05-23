009dfc22d1347eeb7cdc234f5fab4bd671919e32
Anmol <anmolbiswas20@gmail.com>
2024-05-23 17:20:50 +0000

    Circle v1.0

---
+++ circle.c
@@ -0,0 +1,22 @@
+/*
+Author : Anmol Biswas
+Minimum of two numbers
+DOC : 23rd May 2024
+*/
+
+#include <stdio.h>
+#define PI 3.14
+int main()
+{
+    float radius;
+    printf("Enter radius of circle: ");
+    scanf("%f",&radius);
+    printf("Circumference of Circle: %f\n",2*PI*radius);
+    printf("Area of Circle: %f",PI*radius*radius);
+
+
+    return 0;
+}
+
+
+
