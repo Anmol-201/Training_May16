5b1b2dc0bfb8cc9d81ceeafa4f760f6b0ebb0ebb
Anmol <anmolbiswas20@gmail.com>
2024-05-23 17:55:16 +0000

    Grades v1.0

---
+++ grades.c
@@ -0,0 +1,30 @@
+/*
+Author : Anmol Biswas
+Grades
+DOC : 23rd May 2024
+*/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int score;
+    printf("Enter your score: ");
+    scanf("%d",&score);
+
+    if(score>=90 && score <=100)
+    printf("Grade : A");
+    else if(score>=80 && score <=89)
+    printf("Grade : B");
+    else if(score>=70 && score <=79)
+    printf("Grade : C");
+    else if(score>=60 && score <=69)
+    printf("Grade : D");
+    else
+    printf("Grade : E");
+
+    return 0;
+}
+
