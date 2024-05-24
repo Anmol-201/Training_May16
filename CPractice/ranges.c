5b72fddb3fb61219a00db53f35132ada63001c93
Anmol <anmolbiswas20@gmail.com>
2024-05-24 04:12:28 +0000

    Moved files to a folder

---
+++ CPractice/ranges.c
@@ -0,0 +1,24 @@
+/*
+Author : Anmol Biswas
+Size ranges
+DOC : 23rd May 2024
+*/
+
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+ 
+int main() {
+  	
+     printf("CHAR SIZE: %d\t", UCHAR_MAX/8);
+     printf("INT SIZE: %d\t", UINT_MAX/8);
+     printf("LONG INT SIZE: %d\t", ULONG_MAX/8);
+     printf("FLOAT SIZE: %d\t", FLT_MAX/8);
+     printf("DOUBLE SIZE: %d\t", DBL_MAX/8);
+
+
+     return 0;
+}
+
+
+
