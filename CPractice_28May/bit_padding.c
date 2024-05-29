1de63f3b725a973abab84465da639f1515d1eb2f
Anmol <anmolbiswas20@gmail.com>
2024-05-29 04:59:55 +0000

    Bit fielding & padding

---
+++ CPractice_28May/bit_padding.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct
+ {
+   char a;   // 1 byte
+   int  b;   // 4 bytes
+ } temp1;
+
+typedef struct
+{
+  char a;   // 1 byte
+  int  b;   // 4 bytes
+  char c;   // 1 byte
+} temp2;
+
+
+typedef struct
+{
+  char a;   // 1 byte
+  char c;   // 1 byte
+  int  b;   // 4 bytes
+ } temp3;
+ 
+ 
+ 
+int main()
+{
+  printf("sizeof(structure temp) = %lu\n", sizeof(temp1));
+  printf("sizeof(structure temp) = %lu\n", sizeof(temp2));
+  printf("sizeof(structure temp) = %lu\n", sizeof(temp3));
+  
+  return 0;
+}
